3d99d095ed371a37d2d5e20af8c8c0a7ca696f00
Stan Iliev <stililiev@gmail.com>
2019-01-29 19:45:15 -0500

    Algo 2.0

--- src/depth_obs.cpp
+++ src/depth_obs.cpp
@@ -37,6 +37,8 @@
 #include <sensor_msgs/image_encodings.h>
 //#include "beginner_tutorials/Obstacle.h"
 #include <cmath>
+#include <queue>
+#include <vector>
 #include "drivebase/Obstacle.h"
 
 typedef sensor_msgs::PointCloud2 PointCloud;
@@ -74,6 +76,145 @@ struct MyPoint {
 
 int framesReceived = 0;
 ros::Publisher obs_pub;
+
+
+const int max_obstacle_count = 3;
+const float min_distance_from_robot = 2.00f;
+const float min_xy_distance_between_obstacles = 1.0f;
+
+const float min_distance_from_robot_sqr = min_distance_from_robot*min_distance_from_robot;
+
+
+struct Obstacle2
+{
+	float distance_sqr;
+	float x;
+	float y;
+	float z;
+};
+
+/*template <class T, class S, class C>
+S& Container(std::priority_queue<T, S, C>& q) {
+    struct HackedQueue : private std::priority_queue<T, S, C> {
+        static S& Container(std::priority_queue<T, S, C>& q) {
+            return q.*&HackedQueue::c;
+        }
+    };
+    return HackedQueue::Container(q);
+}*/
+
+std::vector<Obstacle2> obstaclesv;
+
+void cameraCallback2(const sensor_msgs::PointCloud2ConstPtr& msg) {
+	framesReceived++;
+	if (framesReceived % 30 == 0) {
+		if (msg.get()) {
+			ROS_INFO("cameraCallback %d [%dx%d] point_step:%d row_step:%d is_dense:%d stamp:%lld seq:%d frame_id:%s",
+					framesReceived,
+					(int) msg->width, (int)msg->height,
+					(int) msg->point_step, (int)msg->row_step,
+					(int) msg->is_dense,
+					msg->header.stamp.toNSec(), msg->header.seq, msg->header.frame_id.c_str());
+
+			MyPoint* fp = (MyPoint*)msg->data.data();
+			int ystep = (msg->row_step - msg->point_step*msg->width)/msg->point_step;
+			//auto cmp = [](Obstacle2& left, Obstacle2& right) { return left.distance_sqr < right.distance_sqr; };
+			//std::priority_queue<Obstacle2, std::vector<Obstacle2>, decltype(cmp)> obstacles(cmp);
+			//std::vector<Obstacle2> &obstaclesv = Container(obstacles);
+			obstaclesv.clear();
+
+			for (int y = 0; y < msg->height; y++, fp += ystep) {
+				for (int x = 0; x < msg->width; x++, fp++) // {
+					// Some cameras (Kinect) give NAN pixels, so just ignore them
+					if (!(std::isnan(fp->x) || std::isnan(fp->y) || std::isnan(fp->z))) {
+						// Calculate sqr(distance) from camera. Square root is applied before sending on the network.
+						float camera_distance_sqr = fp->x*fp->x + fp->y*fp->y + fp->z*fp->z;
+						if (camera_distance_sqr <= 0.0f || camera_distance_sqr > min_distance_from_robot_sqr) {
+							continue;
+						}
+
+						/*int obstacle_count = obstacles.size();
+						if (obstacle_count == 0) {
+							//this is the very first obstacle - take it.
+							obstacles.push({ camera_distance_sqr, fp->x, fp->y, fp->z});
+							continue;
+						}
+						if (camera_distance_sqr < obstacles.top().distance_sqr) {
+							obstacles.push({ camera_distance_sqr, fp->x, fp->y, fp->z });
+						}
+						if (obstacles.size() > max_obstacle_count) {
+							obstacles.pop();
+						}*/
+
+						// 1. Find closest point in the array to the current point. Closest is defined only by looking linear distance on x and y axis.
+						// 2. Compare closest point distance to camera with current point "fp" distance to camera.
+						// 3. If "fp" is closer to camera then: either replace (if points are too close or array full) or add to "obstaclesv" array.
+						// 4. if "fp" is not closer to camera, but it is far enough from closet point and there is space in array, add to "obstaclesv".
+						// That algorithm does not guarantee at all minimum distance between points is bigger than min_xy_distance_between_obstacles.
+						auto closest_point = obstaclesv.begin();
+						float closest_xy_distance = 1000000000.0f;
+						for (auto it = obstaclesv.begin(); it != obstaclesv.end(); it++) {
+							float current_xy_distance = std::fabs(it->x - fp->x) + std::fabs(it->y - fp->y);
+							if (current_xy_distance < closest_xy_distance) {
+								closest_point = it;
+								closest_xy_distance = current_xy_distance;
+							}
+						}
+						if (closest_point != obstaclesv.end()) {
+							if (closest_point->distance_sqr > camera_distance_sqr) {
+								// if the current point "fp" is closer to camera, we need to either replace or insert it into the array.
+								if (closest_xy_distance < min_xy_distance_between_obstacles || obstaclesv.size() >= max_obstacle_count) {
+									//if points are too close or array full, then replace closest point with "fp"
+									closest_point->x = fp->x;
+									closest_point->y = fp->y;
+									closest_point->z = fp->z;
+									closest_point->distance_sqr = camera_distance_sqr;
+									// Note that here we replace the closest_point without checking if every other point in obstaclesv pis
+									// at least min_xy_distance_between_obstacles away.
+									// TODO: if min_xy_distance_between_obstacles needs to be strictly enforced, then at this point
+									// iterate all obstaclesv and find points that are closer than min_xy_distance_between_obstacles.
+									// If there are such points, either delete them or delete "this" (depending which one is closer to camera).
+								} else {
+									obstaclesv.push_back({ camera_distance_sqr, fp->x, fp->y, fp->z});
+								}
+							} else {
+								// if the current point "fp" is further away from camera, but it is far enough from "closest_point"
+								// and array is not full, take it
+								if (closest_xy_distance > min_xy_distance_between_obstacles && obstaclesv.size() < max_obstacle_count) {
+									obstaclesv.push_back({ camera_distance_sqr, fp->x, fp->y, fp->z});
+								} else {
+									// "fp" is not better - don't use it.
+								}
+							}
+						} else {
+							//this is the very first obstacle - take it.
+							obstaclesv.push_back({ camera_distance_sqr, fp->x, fp->y, fp->z});
+						}
+					}
+			}
+
+			drivebase::Obstacle omsg;
+			omsg.header.stamp = msg->header.stamp;
+			omsg.header.frame_id = msg->header.frame_id;
+			for (auto& obstacle : obstaclesv) {
+				float min_distance = std::sqrt(obstacle.distance_sqr);
+				if (min_distance>0) {
+					omsg.dx = obstacle.x/min_distance;
+					omsg.dy = obstacle.y/min_distance;
+					omsg.dz = obstacle.z/min_distance;
+					omsg.range = min_distance;
+					obs_pub.publish(omsg);
+				}
+				ROS_INFO("point MIN[%.2fx%.2fx%.2f] %.2f",
+						obstacle.x, obstacle.y, obstacle.z, min_distance
+						);
+			}
+		} else {
+			ROS_INFO("cameraCallback %d", framesReceived);
+		}
+	}
+}
+
 void cameraCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
 	//ROS_INFO("I heard: [%s]", msg->data.c_str());
@@ -250,7 +391,9 @@ int main(int argc, char **argv)
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 // %EndTag(SUBSCRIBER)%
 
-  ros::Subscriber sub_camera = n.subscribe("/camera/depth_registered/points", 1, cameraCallback);
+  // topic for realsense "/camera/depth/color/points"
+  // topic for kinect "/camera/depth_registered/points"
+  ros::Subscriber sub_camera = n.subscribe("/camera/depth_registered/points", 1, cameraCallback2);
 
   //roslaunch realsense2_camera rs_rgbd.launch
   //ros::Subscriber sub_camera = n.subscribe("/camera/depth/color/points", 1, cameraCallback);
@@ -265,6 +408,7 @@ int main(int argc, char **argv)
    */
 // %Tag(SPIN)%
   ros::spin();
+
 // %EndTag(SPIN)%
 
   return 0;
